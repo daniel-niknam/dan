81692e8cd31624de11f075527419d3358f25cbe5
Daniel Niknam <daniel-niknam@users.noreply.github.com>
2023-01-09 22:26:31 +1100

    Add a simple scenario for rendering a text and an object

--- lib/dan.rb
+++ lib/dan.rb
@@ -2,7 +2,22 @@
 
 require_relative "dan/version"
 
-module Dan
+class Dan
   class Error < StandardError; end
-  # Your code goes here...
+
+  RUBY_EXPRESION_REGEX = /<%=\s?(?<exp>[a-z0-9()]+)\s?%>/i
+  private_constant :RUBY_EXPRESION_REGEX
+
+  def initialize(string)
+    @string = string
+    @output = ""
+  end
+
+  def result(b)
+    @output = @string.gsub(RUBY_EXPRESION_REGEX) do |_m|
+      b.eval($~[:exp])
+    end
+
+    @output
+  end
 end
--- spec/dan_spec.rb
+++ spec/dan_spec.rb
@@ -4,4 +4,19 @@ RSpec.describe Dan do
   it "has a version number" do
     expect(Dan::VERSION).not_to be nil
   end
+
+  it "renders a simple text" do
+    template = Dan.new <<~EOF
+      This is my text
+    EOF
+    expect(template.result(binding)).to eq("This is my text\n")
+  end
+
+  it "renders a simple object in the given template" do
+    x = 42
+    template = Dan.new <<~EOF
+      The value of x is: <%= x %>
+    EOF
+    expect(template.result(binding)).to eq("The value of x is: 42\n")
+  end
 end
